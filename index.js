827fa9e5e728e10a2d57878b0261092a57ddbb86
Pablo Rain <pskyvader@hotmail.com>
2023-10-10 19:55:36 -0500

    a

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const baseFolderPath = "C:\\Users\\pskyv\\Documents\\pico_curtains\\";
+const excludedExtensions = [".gitignore", ".conf", ".md"]; // Add more extensions as needed
+
+function isExcludedFile(file) {
+	const fileName = path.basename(file);
+	const extension = path.extname(file).toLowerCase();
+
+	const excludedFiles = [".gitignore", "LICENSE"]; // Add more files as needed
+	const excludedFolders = [".git"]; // Add more folders as needed
+
+	return (
+		excludedFiles.includes(fileName) ||
+		excludedFolders.includes(fileName) ||
+		excludedExtensions.includes(extension)
+	);
+}
+
+function getFilesRecursive(folderPath, baseURL) {
+	let files = [];
+	const entries = fs.readdirSync(folderPath, { withFileTypes: true });
+
+	entries.forEach((entry) => {
+		const fullPath = path.join(folderPath, entry.name);
+
+		if (entry.isFile()) {
+			if (!isExcludedFile(fullPath)) {
+				const relativePath = path
+					.relative(baseFolderPath, fullPath)
+					.replace(/\\/g, "/"); // Replace backslashes with forward slashes
+				const fileURL = new URL(relativePath, baseURL).href;
+				files.push(fileURL);
+			}
+		} else if (entry.isDirectory()) {
+			if (!isExcludedFile(fullPath)) {
+				const subfolderFiles = getFilesRecursive(fullPath, baseURL);
+				files = files.concat(subfolderFiles);
+			}
+		}
+	});
+
+	return files;
+}
+
+app.get("/", (req, res) => {
+	const baseURL = req.protocol + "://" + req.get("host");
+	const files = getFilesRecursive(baseFolderPath, baseURL);
+	res.json(files);
+});
+
+app.get("/*", (req, res) => {
+	const filePath = path.join(baseFolderPath, req.params[0]);
+
+	if (fs.existsSync(filePath)) {
+		res.download(filePath, (err) => {
+			if (err) {
+				console.error(err);
+				res.status(500).json({ error: "Internal Server Error" });
+			}
+		});
+	} else {
+		res.status(404).json({ error: "File Not Found" });
+	}
+});
+
+app.listen(3000, () => {
+	console.log("Server is running on port 3000");
+});
